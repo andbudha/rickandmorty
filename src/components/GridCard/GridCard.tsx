3499789194a598613a06f512384c921f6f4fcf88
andbudha <andreibartov@gmail.com>
2024-05-04 16:21:12 +0200

    GridCard comp created &styled

---
+++ src/components/GridCard/GridCard.tsx
@@ -0,0 +1,16 @@
+import styles from './GridCard.module.scss';
+
+export const GridCard = () => {
+  return (
+    <div className={styles.gridcard_main_box}>
+      <div className={styles.gridcard_box}>
+        <h1>Card</h1>
+        <h1>Card</h1>
+        <h1>Card</h1>
+        <h1>Card</h1>
+        <h1>Card</h1>
+        <h1>Card</h1>
+      </div>
+    </div>
+  );
+};
