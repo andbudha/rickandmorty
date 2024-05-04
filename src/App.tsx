c209f8cc27ded2e382028d552f34e0ea08104569
andbudha <andreibartov@gmail.com>
2024-05-04 17:32:43 +0200

    data fetched in the App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
 import { Footer } from './components/Footer/Footer';
+import { GridCard } from './components/GridCard/GridCard';
+import { FetchingDataResponse, Result } from './assets/types/types';
+import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState<null | Result[]>(null);
+  console.log(characters);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get<FetchingDataResponse>(
+        ' https://rickandmortyapi.com/api/character/'
+      );
+      const data = response.data.results;
+      setCharacters(data);
+    };
+    getData();
+  }, []);
   return (
     <div className={styles.main_app_box}>
       <div className={styles.app_box}>
         {' '}
         <Footer />
+        <GridCard characters={characters} />
       </div>
     </div>
   );
