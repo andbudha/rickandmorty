652e6850e7da12af34418db7390f701fc98aeb70
andbudha <andreibartov@gmail.com>
2024-05-04 17:39:55 +0200

    GridCard and Card comp-styles updated

--- src/components/GridCard/GridCard.tsx
+++ src/components/GridCard/GridCard.tsx
@@ -1,16 +1,19 @@
+import { Result } from '../../assets/types/types';
+import { Card } from '../Card/Card';
 import styles from './GridCard.module.scss';
 
-export const GridCard = () => {
+type GridCard = {
+  characters: Result[] | null;
+};
+export const GridCard = ({ characters }: GridCard) => {
+  const cards = characters?.map((character) => (
+    <div key={character.id}>
+      <Card character={character} />
+    </div>
+  ));
   return (
     <div className={styles.gridcard_main_box}>
-      <div className={styles.gridcard_box}>
-        <h1>Card</h1>
-        <h1>Card</h1>
-        <h1>Card</h1>
-        <h1>Card</h1>
-        <h1>Card</h1>
-        <h1>Card</h1>
-      </div>
+      <div className={styles.gridcard_box}>{cards}</div>
     </div>
   );
 };
