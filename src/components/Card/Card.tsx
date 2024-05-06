9f315f8e6b8a5277311ad32b38b003fe88266da8
andbudha <andreibartov@gmail.com>
2024-05-06 13:56:31 +0200

    card comp updated&restyled - flip card backside added

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,10 +5,31 @@ interface Card {
   character: Result;
 }
 export const Card = ({ character }: Card) => {
+  const displayCharacterInfoHandler = (characterID: number) => {
+    console.log(characterID);
+  };
   return (
     <div className={styles.card_main_box}>
       <div className={styles.card_box}>
-        <img className={styles.character_image} src={character.image} alt="" />
+        {/* <div className={styles.flip_card_front_main_box}>
+          <img
+            className={styles.character_image}
+            src={character.image}
+            alt=""
+          />
+        </div> */}
+        <div className={styles.flip_card_back_main_box}>
+          <div className={styles.flip_card_back_box}>
+            {' '}
+            <h3 className={styles.character_name}>{character.name}</h3>
+            <div
+              className={styles.read_more_btn}
+              onClick={() => displayCharacterInfoHandler(character.id)}
+            >
+              Read More
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
